6260f87fc4fc69bebee83142a78c9d38d474d5a9
Tess SANTONI <tsantoni@bess-f3r4s8.clusters.42paris.fr>
2023-12-21 17:09:57 +0100

    add exceptions

--- predict.py
+++ predict.py
@@ -6,27 +6,30 @@ def predict(x, t0, t1):
     return (t0 + t1 * x)
 
 def main():
-    print(f"{Colors.INFO}[INFO]    Welcome to this car price prediction tool.{Colors.RES}")
-    thetas = [0, 0]
-    if os.path.isfile("thetas.npy"):
-        thetas = np.load("thetas.npy")
-    else:
-        print(f"{Colors.WARNING}[WARNING] It has not been trained yet.{Colors.RES}")
-    print(f"{Colors.INFO}[INFO]    Thetas are {thetas[0]} and {thetas[1]}{Colors.RES}\n")
-    while True:
-        try:
-            prompt = input(f"{Colors.PINK}>> Please, enter a mileage: {Colors.RES}")
-            if prompt == "exit":
-                exit()
-            x = int(prompt)
-            if x < 0:
-                print(f"{Colors.WARNING}[WARNING] A car should not have a negative mileage.{Colors.RES}")
-            y = predict(x, thetas[0], thetas[1])
-            if y < 0:
-                print(f"{Colors.WARNING}[WARNING] A car with an excessive mileage results in a negative predicted price.{Colors.RES}")
-            print(f"{Colors.RES}The estimated price for a car that has a mileage of {Colors.PINK}{x}{Colors.RES} (km) is {Colors.PINK}{y:.2f}{Colors.RES}.\n")
-        except ValueError:
-            print(f"{Colors.RED}Error: {Colors.RES}The mileage should be a natural number. If you are done, type \"exit\" to quit the program.\n")
+    try:
+        print(f"{Colors.INFO}[INFO]    Welcome to this car price prediction tool.{Colors.RES}")
+        thetas = [0, 0]
+        if os.path.isfile("thetas.npy"):
+            thetas = np.load("thetas.npy")
+        else:
+            print(f"{Colors.WARNING}[WARNING] It has not been trained yet.{Colors.RES}")
+        print(f"{Colors.INFO}[INFO]    Thetas are {thetas[0]} and {thetas[1]}{Colors.RES}\n")
+        while True:
+            try:
+                prompt = input(f"{Colors.PINK}>> Please, enter a mileage: {Colors.RES}")
+                if prompt == "exit":
+                    exit()
+                x = int(prompt)
+                if x < 0:
+                    print(f"{Colors.WARNING}[WARNING] A car should not have a negative mileage.{Colors.RES}")
+                y = predict(x, thetas[0], thetas[1])
+                if y < 0:
+                    print(f"{Colors.WARNING}[WARNING] A car with an excessive mileage results in a negative predicted price.{Colors.RES}")
+                print(f"{Colors.RES}The estimated price for a car that has a mileage of {Colors.PINK}{x}{Colors.RES} (km) is {Colors.PINK}{y:.2f}{Colors.RES}.\n")
+            except ValueError:
+                print(f"{Colors.RED}Error: {Colors.RES}The mileage should be a natural number. If you are done, type \"exit\" to quit the program.\n")
+    except Exception as e:
+        print(e)
 
 if (__name__ == "__main__"):
     main()
--- train.py
+++ train.py
@@ -2,24 +2,27 @@ from data import Data
 from linear_regression import LinearRegression
 
 def main():
-    # Data exploration
-    dt = Data("dataset/data.csv", "km", "price")
-    dt.summary(dt.full_data)
-    dt.scatter(dt.full_data["km"], dt.full_data["price"], "initial")
-    dt.histogram(dt.full_data, 6, "initial")
+    try:
+        # Data exploration
+        dt = Data("dataset/data.csv", "km", "price")
+        dt.summary(dt.full_data)
+        dt.scatter(dt.full_data["km"], dt.full_data["price"], "initial")
+        dt.histogram(dt.full_data, 6, "initial")
 
-    # Linear regression
-    lr = LinearRegression(dt.x, dt.y)
-    lr.launch()
-    lr.plot_line()
+        # Linear regression
+        lr = LinearRegression(dt.x, dt.y)
+        lr.launch()
+        lr.plot_line()
 
-    # Analysis
-    lr.simple_plot(lr.cost, "Cost function", "iterations", "mean squared error")
-    lr.simple_plot(lr.conv, "Convergence", "iterations", "convergence")
-    lr.evaluate()
+        # Analysis
+        lr.simple_plot(lr.cost, "Cost function", "iterations", "mean squared error")
+        lr.simple_plot(lr.conv, "Convergence", "iterations", "convergence")
+        lr.evaluate()
 
-    # Save coefficients
-    lr.save()
+        # Save coefficients
+        lr.save()
+    except Exception as e:
+        print(e)
 
 if (__name__ == "__main__"):
     main()
